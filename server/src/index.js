e74d14232daabe024fd704be00c9c80f2a12d786
tollispa <112690202+tollispa@users.noreply.github.com>
2023-04-11 14:29:03 +0200

    Added cors, express.json and /register route

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const db = require("./database");
+const register = require("./controllers/authenticationRoute/register");
+const cors = require("cors")
+
 
 const app = express();
+app.use(express.json())
+
+app.use(cors({
+  origin: ["http://localhost:3000"],
+  methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
+  credentials: true
+}));
+
 
 db();
 
+app.use("/register", register)
 app.listen(4000, () => {
   console.log("Server started on port 4000");
 });
