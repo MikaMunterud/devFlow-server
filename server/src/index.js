a607110cf86ea97deab95f486b9890d2d861bb17
Noel Perland <noel.perland@yh.nackademin.se>
2023-04-11 16:20:38 +0200

    Skapat login endpoint

--- server/src/index.js
+++ server/src/index.js
@@ -1,22 +1,45 @@
+const session = require("express-session");
 const express = require("express");
 const db = require("./database");
+const login = require("./controllers/authenticationRoute/login");
 const register = require("./controllers/authenticationRoute/register");
-const cors = require("cors")
-
+const cors = require("cors");
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
-app.use(cors({
-  origin: ["http://localhost:3000"],
-  methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
-  credentials: true
-}));
+app.use(
+  session({
+    key: "userId",
+    secret: "mysecret",
+    resave: false,
+    saveUninitialized: false,
 
+    cookie: {
+      domain: "localhost",
+      path: "/",
+      maxAge: 1000 * 60 * 24,
+      httpOnly: true,
+    },
+  })
+);
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
+    credentials: true,
+  })
+);
 
 db();
 
-app.use("/register", register)
+app.use("/register", register);
+app.listen(4000, () => {
+  console.log("Server started on port 4000");
+});
+
+app.use("/login", login);
 app.listen(4000, () => {
   console.log("Server started on port 4000");
 });
